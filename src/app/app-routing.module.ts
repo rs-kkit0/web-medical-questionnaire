9e8fa09cecc2772433a25e3be16b834e3376feca
unknown <rs.kkit0@gmail.com>
2022-10-31 23:21:42 +0900

    add top component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopComponent } from './top/top.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TopComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
